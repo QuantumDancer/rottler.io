58d60d407f8ad2cb210e06fe0f3ed1d6ac0994cf
Benjamin Rottler <benjamin.rottler@cern.ch>
2024-01-04 14:26:25 +0100

    Add DNS record for website

--- infra/route53.tf
+++ infra/route53.tf
@@ -1,7 +1,20 @@
 resource "aws_route53_zone" "website" {
-  name         = local.domain_name
+  name = local.domain_name
 }
 
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route53_record#alias-record
+resource "aws_route53_record" "website" {
+  zone_id = aws_route53_zone.website.zone_id
+  name    = local.domain_name
+  type    = "A"
+  alias {
+    name                   = aws_cloudfront_distribution.website.domain_name
+    zone_id                = aws_cloudfront_distribution.website.hosted_zone_id
+    evaluate_target_health = true
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/acm_certificate_validation#dns-validation-with-route-53
 resource "aws_route53_record" "acm_validation" {
   for_each = {
     for dvo in aws_acm_certificate.website_cert.domain_validation_options : dvo.domain_name => {
