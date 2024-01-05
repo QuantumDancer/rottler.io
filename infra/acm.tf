c24371dbfe47252386c96465dfb6f481578020bc
Benjamin Rottler <benjamin.rottler@cern.ch>
2024-01-05 16:48:59 +0100

    Fix certs for prod env

--- infra/acm.tf
+++ infra/acm.tf
@@ -1,6 +1,11 @@
 resource "aws_acm_certificate" "website_cert" {
-  domain_name       = local.domain_name
-  validation_method = "DNS"
+  # dev:  dev.rottler.io
+  # prod: rottler.io
+  domain_name = var.environment == "prod" ? var.top_level_domain : local.domain_name
+  # dev:  []
+  # prod: www.rottler.io, prod.rottler.io
+  subject_alternative_names = var.environment == "prod" ? ["www.${var.top_level_domain}", local.domain_name] : []
+  validation_method         = "DNS"
 
   lifecycle {
     create_before_destroy = true
